32ca27293db8c14b4f2fc17ca345882fa4cf90c3
Jake Oehler Morrison <jomorrison@gmail.com>
2021-11-12 17:51:04 +0000

    Load the `yoast/phpunit-polyfills` library in testing bootstrap
    This adds the new dependency required by the WordPress 5.8.2 testing suite.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,6 +19,14 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+// Load the PHPUnit Polyfills library.
+$_phpunit_polyfills_lib = dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+if ( ! file_exists( $_phpunit_polyfills_lib ) ) {
+	echo "Could not find $_phpunit_polyfills_lib, have you run `docker-compose up` in order to install Composer packages?" . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
+}
+require_once $_phpunit_polyfills_lib;
+
 /**
  * Manually load the plugin being tested.
  */
