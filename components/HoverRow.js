6d691d87af393d9c5e53ecac812f29cdc86c308c
Kevin Buell <kevin.buell@automattic.com>
2021-12-22 11:59:15 -0500

    Clarify reqs for hover row sections vs table body

--- components/HoverRow.js
+++ components/HoverRow.js
@@ -1,11 +1,11 @@
 /*
 * Hover content includes elements like links or buttons shown when hovering over a row.
-* With help from the design/UX folks, we decided on these requirements for table body rows and hover content:
-* - Hover content is always and immediately shown for, and only for, the row over which the mouse is located
+* With help from the design/UX folks, we decided on these requirements for hover rows and hover content:
+* - Hover content is always and immediately shown for, and only for, the hover row over which the mouse is located
 *     with the following exceptions.
-* - After entering the table body, no new hover content in any row is shown for one second,
+* - After entering a section of hover rows, no new hover content in any row is shown for one second,
 *     though previous content shown on exit might still be shown per the following requirement.
-* - After spending at least one second in the table body then exiting the table body,
+* - After spending at least one second in a section of hover rows then exiting it,
 *     any hover content shown upon exit remains shown for one second.
 * - Hover content can also be shown or hidden via button/keyboard, and
 *     once shown via button/keyboard, hover content remains shown until hidden via button/keyboard.
