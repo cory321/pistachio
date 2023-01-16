477c8ed641688a91fa895f6927c5bcab44651493
Cory Williams <cory321@gmail.com>
2023-01-16 12:50:18 -0800

    Refactor CandidatesContainer to functional component
    Benefits of refactor include the ability to use hooks, enhanced readability, simplicity of code, and performance improvements over the long term

--- containers/Candidates.js
+++ containers/Candidates.js
@@ -1,41 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { compose } from '@wordpress/compose';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { merge } from 'lodash';
 
 import Candidates from '../components/Candidates';
 
 import '@wordpress/core-data';
-import { withSelect } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
 import apiFetch from '@wordpress/api-fetch';
 
-class CandidateContainer extends Component {
-	componentDidUpdate( props ) {
-		if ( this.props.filters !== props.filters ) {
-			this.getCandidateFilters( props );
+const CandidateContainer = props => {
+	const filters = useSelector( state => state.filters );
+	const { candidates, error, isFetching, currentUser } = useSelect( select => ( {
+		candidates: select( 'core' ).getEntityRecords( 'postType', 'candidate', { per_page: 300 } ),
+		error: null,
+		isFetching: false,
+		currentUser: null,
+	} ) );
+
+	useEffect( () => {
+		if ( filters !== props.filters ) {
+			console.log( filters );
 		}
-	}
+	}, [ filters ] );
 
-	getCandidateFilters( props ) {
-		console.log( this.props.filters );
-	}
-
-	render() {
-		return (
-			<Candidates
-				candidates={ this.props.candidates }
-				error={ this.props.error }
-				isFetching={ this.props.isFetching }
-				currentUser={ this.props.currentUser }
-				refresh={ this.props.fetchOne }
-				uploadCoverLetter={ this.props.uploadCoverLetter }
-				addPronouns={ this.addPronouns }
-				filters={ this.props.filters }
-			/>
-		);
-	}
-
-	addPronouns( candidate, pronouns ) {
+	const addPronouns = ( candidate, pronouns ) => {
 		apiFetch( {
 			path: '/wp/v2/candidates/' + candidate.id,
 			method: 'POST',
@@ -49,23 +37,20 @@ class CandidateContainer extends Component {
 				} ),
 			},
 		} );
-	}
-}
-
-const mapStateToProps = state => {
-	return {
-		filters: state.filters,
 	};
+
+	return (
+		<Candidates
+			candidates={ candidates }
+			error={ error }
+			isFetching={ isFetching }
+			currentUser={ currentUser }
+			refresh={ props.fetchOne }
+			uploadCoverLetter={ props.uploadCoverLetter }
+			addPronouns={ addPronouns }
+			filters={ props.filters }
+		/>
+	);
 };
 
-export default compose( [
-	withSelect( select => {
-		return {
-			candidates: select( 'core' ).getEntityRecords( 'postType', 'candidate', { per_page: 300 } ),
-			error: null,
-			isFetching: false,
-			currentUser: null,
-		};
-	} ),
-	connect( mapStateToProps ),
-] )( CandidateContainer );
+export default CandidateContainer;
