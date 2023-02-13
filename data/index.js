04b5051d18fd8a6b916cd19d631f98154679dcd2
Cory Williams <cory321@gmail.com>
2023-02-13 12:00:22 -0800

    Format actions for wp.data

--- data/index.js
+++ data/index.js
@@ -1,4 +1,4 @@
-import * as actions from '../actions';
+import * as nestedActions from '../actions';
 import * as reducers from '../reducers';
 import * as selectors from '../selectors';
 import * as resolvers from '../resolvers/candidates';
@@ -7,6 +7,19 @@ import controls from '../controls';
 import { combineReducers } from '@wordpress/data';
 
 const reducer = combineReducers( reducers );
+
+const actions = {};
+
+// Flatten and format nested actions like this: candidates.addMany -> addManyCandidates
+Object.entries( nestedActions ).forEach( ( [ moduleName, moduleActions ] ) => {
+	const capitalizedModuleName = moduleName.charAt( 0 ).toUpperCase() + moduleName.slice( 1 );
+
+	Object.entries( moduleActions )
+		.filter( ( [ , actionFunction ] ) => typeof actionFunction === 'function' )
+		.forEach( ( [ actionName, actionFunction ] ) => {
+			actions[ `${ actionName }${ capitalizedModuleName }` ] = actionFunction;
+		} );
+} );
 
 export const pistachioConfig = {
 	reducer,
