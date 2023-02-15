b5a208c2d425a26476af1697d83fabf531a706ad
Cory Williams <cory321@gmail.com>
2023-02-15 08:59:55 -0800

    Remove unnecessary selector

--- selectors/auth.js
+++ selectors/auth.js
@@ -1,7 +1,3 @@
 export function getGreenhouseAuth( state ) {
 	return state.auth.greenhouse || [];
 }
-
-export function getGreenhouseAuthId( state ) {
-	return state.auth.greenhouse.id || [];
-}
--- selectors/index.js
+++ selectors/index.js
@@ -1,6 +1,6 @@
 import { getFilters } from './filters';
-import { getGreenhouseAuth, getGreenhouseAuthId } from './auth';
+import { getGreenhouseAuth } from './auth';
 import { getUsers } from './users';
 import { getJobs, isFetchingJobs } from './jobs';
 
-export { getFilters, getGreenhouseAuth, getGreenhouseAuthId, getUsers, getJobs, isFetchingJobs };
+export { getFilters, getGreenhouseAuth, getUsers, getJobs, isFetchingJobs };
